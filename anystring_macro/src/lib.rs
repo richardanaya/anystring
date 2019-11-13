71199e866a97da846a73ac9a38405254b083addc
richard <richard>
2019-11-12 21:34:41 -0800

    fix

--- anystring_macro/src/lib.rs
+++ anystring_macro/src/lib.rs
@@ -5,5 +5,8 @@ use std::str::FromStr;
 
 #[proc_macro_hack]
 pub fn anystring(input: TokenStream) -> TokenStream {
-    TokenStream::from_str(&format!("r#\"{}\"#", input.to_string())).unwrap()
+    let f1 = input.to_string();
+    let f2 = f1.replace("\\","\\\\");
+    let totally_unescaped = f2.replace("\"","\\\"");
+    TokenStream::from_str(&format!("\"{}\"", totally_unescaped)).unwrap()
 }
